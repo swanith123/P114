51e34b3b2991c95a6a591664f2f30ca1baff3874
swanith123 <73668844+swanith123@users.noreply.github.com>
2022-01-17 11:59:28 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+
+function preload(){
+}
+
+function setup(){
+canvas = createCanvas(300, 300);
+canvas.center();
+}
+
+function draw(){
+}
+
+function takePicture(){
+save("filter-web-app-picture.png");
+}
